d7222a753b14906dcd21874110da2c2be5e4c21a
Dorrithe Labuschagne <jou@epos.co.za>
2026-09-23 06:38:30 +0200

    Future Sharp-area: kop, Engelse registrasieskakel, kort skakels

--- public/js/futuresharp-paneel.js
+++ public/js/futuresharp-paneel.js
@@ -142,6 +142,8 @@
         teken_instr();
       }
       $("#fsp-reg-skakel").textContent = REGSKAKEL;
+      // Die Engelse ingang wys 'n Engelse voorskou op WhatsApp.
+      $("#fsp-reg-skakel-en").textContent = data.registrasieskakel_en || REGSKAKEL.replace(/\/registreer$/, "/register");
       teken_lys();
     } catch (f) {
       $("#fsp-lys").innerHTML = '<p class="fsp-leeg">Kon nie die registrasies laai nie: ' + esc(f.message) + "</p>";
